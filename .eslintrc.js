3be1f77afab77939f0de34a61e642058757fe0c4
mattdinata <matt.dinata@gmail.com>
2024-09-11 17:09:34 +0800

    feat: init pre-commit hook and linting

---
+++ .eslintrc.js
@@ -0,0 +1,52 @@
+module.exports = {
+	env: {
+		browser: true,
+		es2021: true,
+		node: true,
+	},
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		ecmaFeatures: {
+			jsx: true,
+		},
+		ecmaVersion: 12,
+		sourceType: 'module',
+	},
+	plugins: ['react', 'prettier', '@typescript-eslint'],
+	extends: [
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:react/recommended',
+		'prettier',
+		'next/core-web-vitals',
+	],
+	rules: {
+		'prettier/prettier': 'error',
+		'import/extensions': [
+			'error',
+			'ignorePackages',
+			{
+				js: 'never',
+				jsx: 'never',
+				ts: 'never',
+				tsx: 'never',
+			},
+		],
+		'react/jsx-filename-extension': [2, { extensions: ['.js', '.jsx', '.ts', '.tsx'] }],
+		'react/prop-types': 'off',
+		'react/require-default-props': 'off',
+		'import/no-extraneous-dependencies': 'off',
+		'react/jsx-no-useless-fragment': 'off',
+		'react/no-array-index-key': 'off',
+		'no-unused-vars': 'off',
+		'@typescript-eslint/no-unused-vars': ['error'],
+	},
+	overrides: [
+		{
+			files: ['**/*.tsx'],
+			rules: {
+				'react/jsx-props-no-spreading': 'off',
+			},
+		},
+	],
+};
