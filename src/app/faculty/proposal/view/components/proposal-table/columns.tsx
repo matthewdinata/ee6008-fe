c0d3c356ec09a7b3c8678f894be50367aedbf2c1
mattdinata <matt.dinata@gmail.com>
2024-12-12 16:47:34 +0700

    feat: add custom badge

--- src/app/faculty/proposal/view/components/proposal-table/columns.tsx
+++ src/app/faculty/proposal/view/components/proposal-table/columns.tsx
@@ -3,6 +3,7 @@
 import { ColumnDef } from '@tanstack/react-table';
 import { MoreHorizontal } from 'lucide-react';
 
+import { Badge } from '@/components/ui/badge';
 import { Button } from '@/components/ui/button';
 import {
 	DropdownMenu,
@@ -11,7 +12,7 @@ import {
 	DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu';
 
-import { Proposal } from './types';
+import { Proposal, ProposalStatus } from './types';
 
 export const columns: ColumnDef<Proposal>[] = [
 	{
@@ -33,6 +34,21 @@ export const columns: ColumnDef<Proposal>[] = [
 	{
 		accessorKey: 'status',
 		header: 'Status',
+		cell: ({ row }) => {
+			const status = row.original.status;
+			let variant: 'outline' | 'outlineSuccess' | 'outlinePending' | 'outlineFail' =
+				'outline';
+
+			if (status === ProposalStatus.APPROVED) {
+				variant = 'outlineSuccess';
+			} else if (status === ProposalStatus.PENDING) {
+				variant = 'outlinePending';
+			} else if (status === ProposalStatus.REJECTED) {
+				variant = 'outlineFail';
+			}
+
+			return <Badge variant={variant}>{status}</Badge>;
+		},
 	},
 	{
 		id: 'actions',
