668d9719374cd3d8468865288065a4be2754ffbc
mattdinata <matt.dinata@gmail.com>
2024-12-12 22:39:54 +0700

    chore: add todo prompt

--- src/app/faculty/proposal/(all)/page.tsx
+++ src/app/faculty/proposal/(all)/page.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import AllProposalTable from './components/all-proposal-table';
 
 function AllProposals() {
+	// TODO: allow only course coordinator or programme director to access
 	return (
 		<div className="mb-8">
 			<AllProposalTable />
