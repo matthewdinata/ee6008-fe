0e09bdacabf593fd37e2a9eb6d51a2b63ddefa49
mattdinata <matt.dinata@gmail.com>
2025-03-19 01:00:58 +0800

    chore: clean up dupe files

--- src/app/utils/validation 2.ts
+++
@@ -1,6 +0,0 @@
-// utils/validation.ts
-
-export const isValidNTUEmail = (email: string): boolean => {
-	const validDomains = ['e.ntu.edu.sg', 'staff.ntu.edu.sg', 'ntu.edu.sg'];
-	return validDomains.some((domain) => email.toLowerCase().endsWith(domain));
-};
